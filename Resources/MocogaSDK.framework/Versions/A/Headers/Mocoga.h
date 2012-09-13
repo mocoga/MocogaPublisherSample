ab9181ae5c16547ebd36f687b0a7a5bab5fadd87
mocoga <dev@mocoga.com>
2012-09-13 15:02:32 +0900

    4inch wide 지원

--- Resources/MocogaSDK.framework/Versions/A/Headers/Mocoga.h
+++ Resources/MocogaSDK.framework/Versions/A/Headers/Mocoga.h
@@ -1,7 +1,7 @@
 //
 //  Mocoga.h
 //  Mocoga Publisher SDK
-//	Version 0.9
+//	Version 0.9.2
 //
 //  Created by dev@mocoga.com Mocoga Development Team on 12. 8. 27.
 //  Copyright (c) 2012 Mocoga, nTels Company. All rights reserved.
